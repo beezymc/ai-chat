9cc2d2ea3d874739dc29d8ecb83bb08e06e1130a
David Rajec <david.rajec@gmail.com>
2022-05-16 11:30:12 -0700

    React app template with MUI5

---
+++ src/pages/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import AbcIcon from '@mui/icons-material/Abc';
+
+const App = () => {
+  return (
+    <>
+      <Button variant='contained'>React App Template</Button>
+      <AbcIcon />
+    </>
+  );
+}
+
+export default App;
