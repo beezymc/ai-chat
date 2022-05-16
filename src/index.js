e8cc2cb3adcd2ab501da51b70de78f07bbf0244f
David Rajec <david.rajec@gmail.com>
2022-05-16 16:30:41 -0700

    Updated template

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
-import App from './pages/App';
+import App from './App';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
